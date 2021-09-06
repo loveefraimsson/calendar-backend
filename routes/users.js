10c42f7cc94395fb89f3c6ecd1738e5a456f1584
loveefraimsson <loveefraimsson@gmail.com>
2021-09-06 16:27:55 +0200

    initial commit

---
+++ routes/users.js
@@ -0,0 +1,98 @@
+var express = require('express');
+var router = express.Router();
+const fs = require("fs");
+const cors = require("cors");
+
+router.use(cors());
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  fs.readFile("toDoList.json", function(err, data) {
+    if (err) {
+      console.log(err);
+    }
+
+    let todo = JSON.parse(data);
+    res.json(todo);
+
+  });
+});
+
+
+// router.post("/delete", function(req, res) {
+//   let title = req.body.title;
+//   console.log("Clicked todo: ", title);
+
+//   //let title = "Laga mat";
+  
+
+//   fs.readFile("toDoList.json", function(err, data) {
+//     if(err) {
+//       console.log(err);
+//     }
+
+//     let list = JSON.parse(data)
+//     console.log("all list:", list);
+    
+//     let findToDo = list.find((list) => list.title === title);
+//     console.log("findToDo", findToDo);
+    
+//     findToDo.done = "true";
+
+//     console.log("list 2:", list);
+    
+//     fs.writeFile("toDoList.json", JSON.stringify(list, null, 2), function() {
+//       if(err) {
+//         console.log(err);
+//       }
+
+//       res.json({list})
+//     })
+    
+//     //res.send(list);
+    
+//   })
+
+// })
+
+
+
+router.post("/delete", function(req, res) {
+  let incommingToDo = req.body.newToDo;
+  console.log("incommingToDo: ", incommingToDo);
+
+    fs.readFile("toDoList.json", function(err, data) {
+      if(err) {
+        console.log(err);
+      }
+
+      let list = JSON.parse(data)
+      console.log("all list:", list);
+      
+      let newToDo = {deadline: "-", title: incommingToDo, done: "false"};
+
+      list.push(newToDo)
+      console.log("newList", list);
+
+      
+      
+
+      
+      
+      fs.writeFile("toDoList.json", JSON.stringify(list, null, 2), function() {
+        if(err) {
+          console.log(err);
+        }
+
+        res.json({list})
+    })
+    
+    //res.send(list);
+    
+  })
+
+
+})
+
+module.exports = router;
