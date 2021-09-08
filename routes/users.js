8a9439e98192b7be61e7a49abb57c267ae4f51d9
loveefraimsson <loveefraimsson@gmail.com>
2021-09-08 14:50:45 +0200

    Functions ready in read, delete and add todo

--- routes/users.js
+++ routes/users.js
@@ -20,46 +20,46 @@ router.get('/', function(req, res, next) {
 });
 
 
-// router.post("/delete", function(req, res) {
-//   let title = req.body.title;
-//   console.log("Clicked todo: ", title);
+router.post("/delete", function(req, res) {
+  let title = req.body.title;
+  console.log("Clicked todo: ", title);
 
-//   //let title = "Laga mat";
+  //let title = "Laga mat";
   
 
-//   fs.readFile("toDoList.json", function(err, data) {
-//     if(err) {
-//       console.log(err);
-//     }
+  fs.readFile("toDoList.json", function(err, data) {
+    if(err) {
+      console.log(err);
+    }
 
-//     let list = JSON.parse(data)
-//     console.log("all list:", list);
+    let list = JSON.parse(data)
+    console.log("all list:", list);
     
-//     let findToDo = list.find((list) => list.title === title);
-//     console.log("findToDo", findToDo);
+    let findToDo = list.find((list) => list.title === title);
+    console.log("findToDo", findToDo);
     
-//     findToDo.done = "true";
+    findToDo.done = "true";
 
-//     console.log("list 2:", list);
+    console.log("list 2:", list);
     
-//     fs.writeFile("toDoList.json", JSON.stringify(list, null, 2), function() {
-//       if(err) {
-//         console.log(err);
-//       }
+    fs.writeFile("toDoList.json", JSON.stringify(list, null, 2), function() {
+      if(err) {
+        console.log(err);
+      }
 
-//       res.json({list})
-//     })
+      res.json({list})
+    })
     
-//     //res.send(list);
+    // res.send(list);
     
-//   })
+  })
 
-// })
+})
 
 
 
-router.post("/delete", function(req, res) {
-  let incommingToDo = req.body.newToDo;
+router.post("/addToDo", function(req, res) {
+  let incommingToDo = req.body;
   console.log("incommingToDo: ", incommingToDo);
 
     fs.readFile("toDoList.json", function(err, data) {
@@ -70,23 +70,24 @@ router.post("/delete", function(req, res) {
       let list = JSON.parse(data)
       console.log("all list:", list);
       
-      let newToDo = {deadline: "-", title: incommingToDo, done: "false"};
-
+      let newToDo = {deadline: incommingToDo.theDeadline , title: incommingToDo.newToDo, done: incommingToDo.done};
+      console.log("newToDo", newToDo);
       list.push(newToDo)
       console.log("newList", list);
 
       
       
+      
 
       
       
-      fs.writeFile("toDoList.json", JSON.stringify(list, null, 2), function() {
+      fs.writeFile("toDoList.json", JSON.stringify(list, null, 2), function() { 
         if(err) {
           console.log(err);
         }
 
         res.json({list})
-    })
+      })
     
     //res.send(list);
     
